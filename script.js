c45ac456cde57b257eaacf1d928f7aee98013097
Simon Furlan <simon.furlan18@gmail.com>
2024-11-10 20:49:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function setGradientBackground() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  let gradient, imageUrl;
+
+  if ((hours > 6 || (hours === 6 && minutes >= 30)) && (hours < 15 || (hours === 15 && minutes < 30))) {
+    gradient = "linear-gradient(to bottom, #87CEEB, #FFFFFF)"; 
+    imageUrl = 'tubes_light.png';
+    document.body.style.color = "#151515";
+  } else {
+    gradient = "linear-gradient(to bottom, #8a2ffc, #000000)";
+    imageUrl = 'tubes_dark.png';
+    document.body.style.color = "white";
+  }
+  document.body.style.background = gradient;
+  const backgroundImageElement = document.querySelector('.background-image');
+  backgroundImageElement.style.backgroundImage = `url('${imageUrl}')`;
+}
+
+function downloadFile() {
+  const fileId = '1gYXQSH3FNzoMDbx8Wg-YBpiHns5-UgWj'; 
+  const downloadUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
+  window.location.href = downloadUrl;
+}
+
+setGradientBackground();
+setInterval(setGradientBackground, 60000);
